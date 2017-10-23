f63ffd4fa334148ff8491852671ab1cc4726363f
julia.timofeeva <i.julia.timofeeva@gmail.com>
2017-10-23 18:47:53 +0300

    Добавление компонентов

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PersonalInformationComponent } from './components/personal-information/personal-information.component';
+import { FriendsGroupsComponent } from './components/friends-groups/friends-groups.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PersonalInformationComponent,
+    FriendsGroupsComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/friends-groups/friends-groups.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-friends-groups',
+  templateUrl: './friends-groups.component.html',
+  styleUrls: ['./friends-groups.component.css']
+})
+export class FriendsGroupsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  @Input() nameColumn;
+
+}
