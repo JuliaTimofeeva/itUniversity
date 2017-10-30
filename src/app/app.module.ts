dd420c9c47a2851f73a11c35bb3f63c21bd8ced6
julia.timofeeva <i.julia.timofeeva@gmail.com>
2017-10-30 17:36:14 +0300

    Создание Routing на странице профиля

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,18 @@ import { HeaderComponent } from './components/header/header.component';
 import { PersonalInformationComponent } from './components/personal-information/personal-information.component';
 import { FriendsGroupsComponent } from './components/friends-groups/friends-groups.component';
 import { FooterComponent } from './components/footer/footer.component';
+import {RouterModule} from "@angular/router";
+import { HomePageComponent } from './home-page/home-page.component';
+import { FriendPageComponent } from './friend-page/friend-page.component';
+import { GroupPageComponent } from './group-page/group-page.component';
+import { WallPageComponent } from './wall-page/wall-page.component';
 
-
+const routes = [
+  {path:'',component: WallPageComponent},
+  {path:'friend-page',component: FriendPageComponent},
+  {path:'group-page', component: GroupPageComponent},
+  {path:'wall-page', component: WallPageComponent}
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,9 +25,14 @@ import { FooterComponent } from './components/footer/footer.component';
     PersonalInformationComponent,
     FriendsGroupsComponent,
     FooterComponent,
+    HomePageComponent,
+    FriendPageComponent,
+    GroupPageComponent,
+    WallPageComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
