285bc114d491fa27221cc42d221962a233832a48
julia.timofeeva <i.julia.timofeeva@gmail.com>
2017-11-17 18:47:49 +0300

    Добавление регистрации пользователя

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -4,7 +4,7 @@ import {UsersService} from "../../users/users.service";
 import {User} from "../../users/user.module";
 import {Message} from "./message.module";
 import {AuthService} from "../auth.sevice";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -19,19 +19,26 @@ export class LoginComponent implements OnInit {
   constructor(
     private usersService: UsersService,
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit() {
     this.message =new Message('danger','');
+    this.route.queryParams.subscribe((params: Params)=>{
+      if(params['nowCanLogin']){
+        this.showMessage({text:"Теперь вы можете зайти в систему",type:'success'})
+      }
+    });
+
     this.form = new FormGroup({
       'email': new FormControl(null, [Validators.required, Validators.email]),
       'password': new FormControl(null, [Validators.required, Validators.minLength(6)])
     });
   }
 
-  private showMessage( text: string, type: string = 'danger'){
-    this.message = new Message(type, text);
+  private showMessage( message: Message){
+    this.message = message;
     window.setTimeout(()=>{
       this.message.text = '';
 
@@ -51,12 +58,18 @@ export class LoginComponent implements OnInit {
             window.localStorage.setItem('user',JSON.stringify(user));
             //logic
             this.authService.login();
-            // this.router.navigate(['']);
+             this.router.navigate(['/wall-page']);
           }else{
-            this.showMessage("Неверный пароль")
+            this.showMessage({
+              text:"Неверный пароль",
+              type: 'danger'
+            })
           }
         }else{
-          this.showMessage("Такого пользователя не существует");
+          this.showMessage({
+            text:"Такого пользователя не существует",
+            type: 'danger'
+          });
         }
       });
 
---
+++ src/app/auth/registration/registration.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl, Validators} from "@angular/forms";
+import {UsersService} from "../../users/users.service";
+import {User} from "../../users/user.module";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css']
+})
+export class RegistrationComponent implements OnInit {
+
+  form: FormGroup;
+
+  constructor(private userService: UsersService,
+              private router: Router) { }
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      'email': new FormControl(null,[Validators.required, Validators.email]),
+      'password': new FormControl(null,[Validators.required, Validators.minLength(6)]),
+      'name': new FormControl(null,[Validators.required]),
+      'agree': new FormControl(false,[Validators.requiredTrue])
+    });
+  }
+
+  onSubmit(){
+    const {email,password, name} = this.form.value;
+    const user = new User(email,password, name);
+    this.userService.createNewUser(user)
+      .subscribe(()=>{
+      this.router.navigate(['/login'],{
+        queryParams: {
+          nowCanLogin: true
+        }
+
+      });
+
+      });
+  }
+
+}
